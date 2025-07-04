0dbf8904c7dd1cf1d013bba8f36c1d40cf3ae4a3
Akhileshal96 <132011661+Akhileshal96@users.noreply.github.com>
2025-07-05 02:57:55 +0530

    Update kite_api.py

--- kite_api.py
+++ kite_api.py
@@ -1,14 +1,12 @@
 # kite_api.py
-from kiteconnect import KiteConnect
 import os
+from kiteconnect import KiteConnect
 
 def get_kite_instance():
     api_key = os.getenv("KITE_API_KEY")
-    access_token = None
-
-    import os
-access_token = os.getenv("ACCESS_TOKEN")
+    access_token = os.getenv("ACCESS_TOKEN")
 
     kite = KiteConnect(api_key=api_key)
     kite.set_access_token(access_token)
+
     return kite
