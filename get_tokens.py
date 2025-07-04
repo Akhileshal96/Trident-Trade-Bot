4209469e59a216a2164a1cf7bfbae5da92e0c60d
Akhileshal96 <132011661+Akhileshal96@users.noreply.github.com>
2025-07-05 02:14:13 +0530

    Create get_tokens.py

---
+++ get_tokens.py
@@ -0,0 +1,20 @@
+# get_tokens.py
+import pandas as pd
+
+def get_token_map_nifty100():
+    url = "https://api.kite.trade/instruments"
+    df = pd.read_csv(url)
+
+    df = df[(df['exchange'] == 'NSE') & (df['instrument_type'] == 'EQ')]
+
+    nifty100_symbols = [
+        "RELIANCE", "INFY", "TCS", "HDFCBANK", "ICICIBANK", "KOTAKBANK",
+        "ITC", "LT", "SBIN", "AXISBANK", "HCLTECH", "WIPRO", "ASIANPAINT",
+        "ULTRACEMCO", "SUNPHARMA", "MARUTI", "TITAN", "BAJAJFINSV", "NESTLEIND",
+        "HINDUNILVR"
+    ]
+
+    df = df[df['tradingsymbol'].isin(nifty100_symbols)]
+
+    token_map = dict(zip(df['tradingsymbol'], df['instrument_token']))
+    return token_map
